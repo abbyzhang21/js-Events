20bee39bd169284b012fb8816fb01bd8c57675cc
Abby Zhang <abbyzhang@abbys-mbp.home>
2018-03-26 14:23:27 -1000

    js-event hw

--- js/app.js
+++ js/app.js
@@ -21,35 +21,69 @@ function lessContent(){
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
-
+function zoom(){
+	var biggieElem = document.getElementById('biggie');
+	biggieElem.style.fontSize = "150%";
+}
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
-
+var menuItems = ["mcferry","icecream","coffee","pie"];
+function valueMenu(){
+	var menuElem = document.getElementById('menu');
+	menuElem.innerHTML = menuItems;
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
-
+function redFace(){
+	var drinkElem = document.getElementById('drink');
+	drinkElem.style.color = "red";
+	drinkElem.style.fontSize = "30px";
+}
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
-
+function showPrice(){
+	var priceElem = document.getElementById('price');
+	priceElem.innerHTML = "$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
+Benjamin.addEventListener('click', showMyQuote);
+function showMyQuote(){
+	var displayQuoteElem = document.getElementById('displayQuote');
+	displayQuoteElem.innerHTML = myQuote;
+}
 
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
-
+//create randomly select from arry
+var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+random.addEventListener('click', showRandomQuote);
+function showRandomQuote(){
+	var displayQuotesElem = document.getElementById('displayQuotes');
+	displayQuotesElem.innerHTML = randomQuote;
+}
 
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
 */
+showHide.addEventListener('click', showOfFreedom);
+function showOfFreedom(){
+	var freedomElem = document.getElementById('freedom');
+	if(freedomElem.style.display === 'none'){
+		freedomElem.style.display = 'block';
+	}else{
+		freedomElem.style.display = 'none';
+	}
+}
